e6f81c908f5d93093eabbc6e0aaa7036d9e9bdc8
XavierRomera <91754890+XavierRomera@users.noreply.github.com>
2021-10-17 16:47:33 +0200

    Add files via upload

---
+++ Ejercicioguia1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Longitud = new System.Windows.Forms.RadioButton();
+            this.Bonito = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.nuevo = new System.Windows.Forms.RadioButton();
+            this.Mayusculas = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // IP
+            // 
+            this.IP.Location = new System.Drawing.Point(112, 64);
+            this.IP.Margin = new System.Windows.Forms.Padding(4);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(172, 22);
+            this.IP.TabIndex = 2;
+            this.IP.Text = "192.168.56.102";
+            this.IP.TextChanged += new System.EventHandler(this.IP_TextChanged);
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(155, 38);
+            this.nombre.Margin = new System.Windows.Forms.Padding(4);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(217, 22);
+            this.nombre.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 200);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Enviar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.Mayusculas);
+            this.groupBox1.Controls.Add(this.nuevo);
+            this.groupBox1.Controls.Add(this.Longitud);
+            this.groupBox1.Controls.Add(this.Bonito);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.nombre);
+            this.groupBox1.Location = new System.Drawing.Point(11, 255);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(484, 249);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Peticion";
+            // 
+            // Longitud
+            // 
+            this.Longitud.AutoSize = true;
+            this.Longitud.Location = new System.Drawing.Point(155, 112);
+            this.Longitud.Margin = new System.Windows.Forms.Padding(4);
+            this.Longitud.Name = "Longitud";
+            this.Longitud.Size = new System.Drawing.Size(220, 21);
+            this.Longitud.TabIndex = 7;
+            this.Longitud.TabStop = true;
+            this.Longitud.Text = "Dime la longitud de mi nombre";
+            this.Longitud.UseVisualStyleBackColor = true;
+            this.Longitud.CheckedChanged += new System.EventHandler(this.Longitud_CheckedChanged);
+            // 
+            // Bonito
+            // 
+            this.Bonito.AutoSize = true;
+            this.Bonito.Location = new System.Drawing.Point(155, 84);
+            this.Bonito.Margin = new System.Windows.Forms.Padding(4);
+            this.Bonito.Name = "Bonito";
+            this.Bonito.Size = new System.Drawing.Size(207, 21);
+            this.Bonito.TabIndex = 8;
+            this.Bonito.TabStop = true;
+            this.Bonito.Text = "Dime si mi nombre es bonito";
+            this.Bonito.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(588, 154);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 63);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Desconectar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // nuevo
+            // 
+            this.nuevo.AutoSize = true;
+            this.nuevo.Location = new System.Drawing.Point(152, 141);
+            this.nuevo.Margin = new System.Windows.Forms.Padding(4);
+            this.nuevo.Name = "nuevo";
+            this.nuevo.Size = new System.Drawing.Size(199, 21);
+            this.nuevo.TabIndex = 10;
+            this.nuevo.TabStop = true;
+            this.nuevo.Text = "Tu nombre es palindromo?";
+            this.nuevo.UseVisualStyleBackColor = true;
+            this.nuevo.CheckedChanged += new System.EventHandler(this.nuevo_CheckedChanged);
+            // 
+            // Mayusculas
+            // 
+            this.Mayusculas.AutoSize = true;
+            this.Mayusculas.Location = new System.Drawing.Point(152, 170);
+            this.Mayusculas.Margin = new System.Windows.Forms.Padding(4);
+            this.Mayusculas.Name = "Mayusculas";
+            this.Mayusculas.Size = new System.Drawing.Size(196, 21);
+            this.Mayusculas.TabIndex = 11;
+            this.Mayusculas.TabStop = true;
+            this.Mayusculas.Text = "Tu nombre en mayusculas";
+            this.Mayusculas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 692);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IP;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Longitud;
+        private System.Windows.Forms.RadioButton Bonito;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton nuevo;
+        private System.Windows.Forms.RadioButton Mayusculas;
+    }
+}
+
